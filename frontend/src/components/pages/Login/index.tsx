fa11a8cb58d1eab49a8bacb5cc12840428fbb294
blogSoul <blo9040@gmail.com>
2021-11-03 03:18:36 +0900

    feat: Page 단위 로그인 창 디자인 구현 완료
    /login에 접속하면 볼 수 있는 로그인 창 디자인 구현을 완료함. 아직 atomic design 적용하지 않음.

--- frontend/src/components/pages/Login/index.tsx
+++ frontend/src/components/pages/Login/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import Label from '@atoms/Label';
+import LabeledButton from '@atoms/LabeledButton';
+import Input from '@atoms/Input';
+import { Container, Layout, LabelContainer, LabelColumn } from './style';
 
 const Login = (): JSX.Element => {
   return (
-    <>
-      <h2>Homes</h2>
-    </>
+    <Layout>
+      <Container>
+        <LabelContainer>
+          <Label text="booslack" width={240} height={20} />
+        </LabelContainer>
+        <LabeledButton
+          text="Github으로 로그인"
+          width={500}
+          backgroundColor="#C8C7EF"
+        />
+        <Input placeholder="  example@email.com" width={500} height={50} />
+        <LabeledButton text="이메일으로 로그인" width={500} />
+        <LabelColumn>
+          <LabeledButton text="회원 가입" width={240} />
+          <LabeledButton text="비밀 번호 찾기" width={240} />
+        </LabelColumn>
+      </Container>
+    </Layout>
   );
 };
 
--- frontend/src/style.ts
+++ frontend/src/style.ts
@@ -45,6 +45,7 @@ footer, header, hgroup, menu, nav, section {
 }
 * {
     font-family: 'Pretendard-Light';
+  font-size: 22px;
 }
 body {
     line-height: 1;
