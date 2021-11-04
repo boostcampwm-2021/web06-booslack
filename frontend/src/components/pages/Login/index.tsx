5ca452714204b2c4b11fda0ad1bcf40d05c69217
blogSoul <blo9040@gmail.com>
2021-11-04 11:25:59 +0900

    fix: dev rebase

--- frontend/src/components/pages/Login/index.tsx
+++ frontend/src/components/pages/Login/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Label from '@atoms/Label';
-import LoginContent from '@organisms/LoginContent';
-import { Container, Layout, LabelContainer } from './style';
+import LabeledButton from '@atoms/LabeledButton';
+import Input from '@atoms/Input';
+import { Container, Layout, LabelContainer, LabelColumn } from './style';
 
 const Login = (): JSX.Element => {
   return (
@@ -10,7 +11,17 @@ const Login = (): JSX.Element => {
         <LabelContainer>
           <Label text="booslack" width={240} height={20} />
         </LabelContainer>
-        <LoginContent />
+        <LabeledButton
+          text="Github으로 로그인"
+          width={500}
+          backgroundColor="#C8C7EF"
+        />
+        <Input placeholder="  example@email.com" width={500} height={50} />
+        <LabeledButton text="이메일으로 로그인" width={500} />
+        <LabelColumn>
+          <LabeledButton text="회원 가입" width={240} />
+          <LabeledButton text="비밀 번호 찾기" width={240} />
+        </LabelColumn>
       </Container>
     </Layout>
   );
