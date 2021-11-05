6be3d29f285898f3053df326a209adb84e15da7c
j166_이충헌 <ycp998@naver.com>
2021-11-05 22:49:49 +0900

    feat: custom hook - useasync 제작

---
+++ frontend/src/hooks/useAsync.ts
@@ -0,0 +1,67 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+interface Data {
+  data?: unknown;
+}
+
+interface TypeHook {
+  data?: unknown;
+  loading: boolean;
+  error: null | Error;
+}
+
+type METHOD = 'GET' | 'POST' | 'DELETE' | 'UPDATE';
+
+const getAxios = async (Path: string, JSON: Data): Promise<Data> => {
+  return axios.get(Path, JSON);
+};
+
+const postAxios = async (Path: string, JSON: Data): Promise<Data> => {
+  return axios.get(Path, JSON);
+};
+
+const deleteAxios = (Path: string, JSON: Data): Promise<Data> => {
+  return axios.get(Path, JSON);
+};
+
+const updateAxios = async (Path: string, JSON: Data): Promise<Data> => {
+  return axios.get(Path, JSON);
+};
+
+const getAxiosByMethod = (method: METHOD) => {
+  if (method === 'POST') return postAxios;
+  if (method === 'DELETE') return deleteAxios;
+  if (method === 'UPDATE') return updateAxios;
+  return getAxios;
+};
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function useAsync(json: Data, path: string, deps = [], method: METHOD = 'GET') {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error>(null);
+
+  const AxiosFunction = getAxiosByMethod(method);
+
+  const fetchRequest = async (): Promise<void> => {
+    setError(null);
+    setData(null);
+    setLoading(true);
+    try {
+      const response = await AxiosFunction(path, json);
+      setData(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchRequest();
+  }, deps);
+
+  return [data, loading, error, fetchRequest];
+}
+
+export default useAsync;
