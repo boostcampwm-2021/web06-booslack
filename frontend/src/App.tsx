f70d589a9467feb010d3b6c41695afeeb50130cc
laz <danier123@gmail.com>
2021-11-08 00:01:20 +0900

    refactor: 리코일 App.tsx로 이동

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,28 +6,33 @@ import BrowseChannel from '@pages/BrowseChannel';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Signup from '@pages/Signup';
 import Changepassword from '@pages/Changepassword';
+import { RecoilRoot } from 'recoil';
+import GlobalStyle from './style';
 
 const App = (): JSX.Element => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route exact path="/client/:channelId">
-          <Workspace />
-        </Route>
-        <Route path="/signup">
-          <Signup />
-        </Route>
-        <Route path="/changepassword">
-          <Changepassword />
-        </Route>
-        <Route path="/browsechannel">
-          <BrowseChannel />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <RecoilRoot>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/client/:channelId">
+            <Workspace />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+          <Route path="/changepassword">
+            <Changepassword />
+          </Route>
+          <Route path="/browsechannel">
+            <BrowseChannel />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </RecoilRoot>
   );
 };
 
