fc9e2f2cf160f0521140a6355a5f2d9d15a90026
loin3 <ya6675@naver.com>
2021-11-11 17:28:16 +0900

    feat: 하단 input 바 블록태그 가능하도록 부분구현
    하단의 input 바를 슬랙 스타일로 구현하는데 리스트, 코드블록, 인용문구과
    같은 블록 태그에 대해 구현

--- frontend/src/components/organisms/ChatInputBackground/styles.ts
+++ frontend/src/components/organisms/ChatInputBackground/styles.ts
@@ -1,18 +1,104 @@
 import styled from 'styled-components';
 
-interface Props {
-  width: number;
-  height: number;
-}
+export const Container = styled.div`
+  padding: 0px 20px;
 
-export const Container = styled.div<Props>`
-  display: flex;
-  width: inherit;
-  height: 10vh;
+  #input-bar {
+    padding: 8px 8px;
+    border: 1px solid black;
+    border-radius: 4px;
+    box-sizing: border-box;
+    cursor: text;
+    line-height: 1.42;
+    height: 100%;
+    outline: none;
+    overflow-y: auto;
+    padding: 12px 15px;
+    tab-size: 4
+    text-align: left;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+  }
 
-  align-items: center;
-  border: 0;
-  background-color: pink;
+  ul,
+  ol {
+    padding-inline-start: 26px;
+  }
+
+  blockquote {
+    position: relative;
+    display: block;
+
+    padding-left: 16px;
+    margin-bottom: 4px;
+    margin-left: 4px;
+  }
+
+  blockquote + blockquote {
+    margin-top: -6px;
+    padding-top: 3px;
+  }
+
+  blockquote::before {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    display: block;
+    width: 4px;
+    content: '';
+    border-radius: 8px;
+    background-color: #dddddd;
+  }
+
+  .ql-code-block + .ql-code-block {
+    margin-top: -14px;
+  }
+
+  .ql-code-block {
+    position: relative;
+    border-left: 1px solid #dddddd;
+    border-right: 1px solid #dddddd;
+    padding-left: 8px;
+    padding-right: 8px;
+    margin-bottom: 14px;
+    background: #f8f8f8;
+    font-family: Monaco, Menlo, Consolas, Courier New, monospace !important;
+    font-size: 12px;
+    line-height: 1.50001;
+    font-variant-ligatures: none;
+    white-space: pre;
+    word-wrap: break-word;
+    word-break: normal;
+    tab-size: 4;
+  }
+
+  .ql-code-block::after {
+    content: '';
+    display: block;
+    position: absolute;
+    bottom: -18px;
+    left: -1px;
+    width: 100%;
+    height: 8px;
+    margin-bottom: 9px;
+    background: #f8f8f8;
+    border-bottom: 1px solid #dddddd;
+    border-left: 1px solid #dddddd;
+    border-right: 1px solid #dddddd;
+    border-bottom-left-radius: 4px;
+    border-bottom-right-radius: 4px;
+    margin-bottom: 10px;
+    width: calc(100% + 2px);
+  }
+
+  #input-bar>.ql-code-block:first-child, #input-bar>:not(.ql-code-block)+.ql-code-block {
+    margin-top: 4px;
+    padding-top: 8px;
+    border-top: 1px solid #dddddd;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+  }
 `;
 
 export default Container;
