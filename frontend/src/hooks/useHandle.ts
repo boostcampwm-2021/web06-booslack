345a7c257f59e4d29bbefd1c080eda7a73a63d1b
Lee <ycp998@naver.com>
2021-11-12 03:21:11 +0900

    feat: useHandle에 쓰로틀 구현

--- frontend/src/hooks/useHandle.ts
+++ frontend/src/hooks/useHandle.ts
@@ -3,10 +3,17 @@ import { useState, useEffect, RefObject } from 'react';
 const useRefLocate = (customRef: RefObject<HTMLElement>): [number, number] => {
   const [xWidth, setWidth] = useState<null | number>(null);
   const [yHeight, setHeight] = useState<null | number>(null);
+  const [throttle, setThrottle] = useState(false);
 
   const handleResize = () => {
-    setWidth(customRef?.current?.getBoundingClientRect()?.x);
-    setHeight(customRef?.current?.getBoundingClientRect()?.y);
+    if (throttle) return;
+
+    setThrottle(true);
+    setTimeout(() => {
+      setWidth(customRef?.current?.getBoundingClientRect()?.x);
+      setHeight(customRef?.current?.getBoundingClientRect()?.y);
+      setThrottle(false);
+    }, 100);
   };
 
   useEffect(() => {
@@ -15,7 +22,7 @@ const useRefLocate = (customRef: RefObject<HTMLElement>): [number, number] => {
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  });
+  }, []);
 
   return [xWidth, yHeight];
 };
