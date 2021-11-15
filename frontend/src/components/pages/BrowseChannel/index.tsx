80049c8d942b30d9b615e98087b0408a6e502210
blogSoul <blo9040@gmail.com>
2021-11-15 15:45:04 +0900

    fix: browserchannel과 client에 대한 오류 수정
    {Page}를 <Page />로 바꾸어서 오류를 해결하였음. {Page}를 함수형으로 받아들여지는 것으로 추측됨 .

--- frontend/src/components/pages/BrowseChannel/index.tsx
+++ frontend/src/components/pages/BrowseChannel/index.tsx
@@ -5,11 +5,24 @@ import WorkspaceTemplate from '@templates/Workspace';
 import { browseCursor } from '@state/Channel';
 
 const BrowseChannel = (): JSX.Element => {
+<<<<<<< HEAD
   return (
     <WorkspaceTemplate>
       <BrowseContent />
     </WorkspaceTemplate>
   );
+=======
+  const Content: JSX.Element = <BrowseContent />;
+
+  const resetCursor = useResetRecoilState(browseCursor);
+  useEffect(() => {
+    return () => {
+      resetCursor();
+    };
+  }, []);
+
+  return <WorkspaceTemplate Content={Content} />;
+>>>>>>> 0b1db23 (fix: browserchannel과 client에 대한 오류 수정)
 };
 
 export default BrowseChannel;
--- frontend/src/components/pages/WorkspaceList/index.tsx
+++ frontend/src/components/pages/WorkspaceList/index.tsx
@@ -1,19 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import WorkspaceListTemplate from '@templates/WorkspaceList';
 import WorkSpaceListContent from '@organisms/WorkspaceListContent';
-import checkIsLogin from '@global/util/CheckIsLogin';
-import { Redirect } from 'react-router-dom';
 import { Container } from './styles';
 
 const WorkspaceList = (): JSX.Element => {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    return () => setLoading(false);
-  }, []);
-  if (!checkIsLogin()) {
-    return <Redirect to="/notlogin" />;
-  }
-
   return (
     <Container>
       <WorkspaceListTemplate>
