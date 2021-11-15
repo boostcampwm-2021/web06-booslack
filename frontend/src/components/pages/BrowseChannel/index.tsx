6d0e175540f3ac83bcadc189d4c7804b2832accf
blogSoul <blo9040@gmail.com>
2021-11-15 15:45:04 +0900

    fix: browserchannel과 client에 대한 오류 수정
    {Page}를 <Page />로 바꾸어서 오류를 해결하였음. {Page}를 함수형으로 받아들여지는 것으로 추측됨 .

--- frontend/src/components/pages/BrowseChannel/index.tsx
+++ frontend/src/components/pages/BrowseChannel/index.tsx
@@ -1,20 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useResetRecoilState } from 'recoil';
 import BrowseContent from '@organisms/BrowseContent';
 import WorkspaceTemplate from '@templates/Workspace';
 import { browseCursor } from '@state/Channel';
-import checkIsLogin from '@global/util/CheckIsLogin';
-import { Redirect } from 'react-router-dom';
 
 const BrowseChannel = (): JSX.Element => {
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    return () => setLoading(false);
-  }, []);
-  if (!checkIsLogin()) {
-    return <Redirect to="/notlogin" />;
-  }
-
   const Content: JSX.Element = <BrowseContent />;
 
   const resetCursor = useResetRecoilState(browseCursor);
