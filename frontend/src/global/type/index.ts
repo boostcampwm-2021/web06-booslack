8959bb38584cecd846d1ceb07045d73fc0e3ad77
laz <danier123@gmail.com>
2021-11-16 00:29:24 +0900

    feat: Workspace, Channel, User 인터페이스 구현

--- frontend/src/global/type/index.ts
+++ frontend/src/global/type/index.ts
@@ -1,3 +1,26 @@
 export type SortOption = 'alpha' | 'rAlpha';
-
 export type METHOD = 'GET' | 'POST' | 'DELETE' | 'UPDATE';
+export type ChannelType = 'public' | 'private';
+export type UserType = 'local' | 'github';
+
+export interface Workspace {
+  id: number;
+  profile?: string;
+  name: string;
+}
+
+export interface Channel {
+  id: number;
+  name: string;
+  type: ChannelType;
+  description?: string;
+  workspaceId: number;
+}
+
+export interface User {
+  id: number;
+  nickname: string;
+  email: string;
+  type: UserType;
+  password: string;
+}
