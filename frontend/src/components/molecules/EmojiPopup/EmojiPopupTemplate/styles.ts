f5f91e87083b02464d9dbd668ee22ce4f4b68a38
laz <75822859+laz@users.noreply.github.com>
2021-11-15 11:15:58 +0900

    feat: 이모티콘 자동완성 팝업 구현 (#106)
    - 성능 상의 문제가 있는 이모티콘 자동완성 구현

---
+++ frontend/src/components/molecules/EmojiPopup/EmojiPopupTemplate/styles.ts
@@ -0,0 +1,36 @@
+import Label from '@atoms/Label';
+import styled from 'styled-components';
+import { RoundScrollBar } from '@global/mixin';
+import { ifProp } from 'styled-tools';
+
+export const Container = styled.div`
+  ${RoundScrollBar};
+  overflow-y: scroll;
+  overflow-x: hidden;
+  width: 100%;
+  max-height: 300px;
+`;
+
+interface Props {
+  selected: boolean;
+}
+
+export const UserContainer = styled.div<Props>`
+  padding: 0 1rem;
+  background-color: ${ifProp({ selected: true }, '#2C639E', 'transparent')};
+`;
+
+export const UserElement = styled.div`
+  height: 32px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const PrimaryContent = styled.div``;
+
+export const SecondaryContent = styled.div``;
+
+export const StyledBoldLabel = styled(Label)`
+  font-weight: bold;
+`;
