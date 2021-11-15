85d8dbce656dfb2a113c2ffda99b397dd5161da2
blogSoul <blo9040@gmail.com>
2021-11-15 09:38:29 +0900

    feat: 로그인 여부 확인 함수 구현 및 모든 페이지 로그인별 페이지 경고 및 라우터 구현
    1. 로그인 여부 확인 함수 구현함.
    2. 로그인이 필요한 페이지는 로그인한 유저만 들어올 수 있도록 하고 로그인이 필요하지 않은 페이지는 로그인 안한 유저만 들어올 수 있도록 라우터 구현.
    
    에러에 대한 참고 링크 :
    https://velog.io/@ohgoodkim/-%EC%97%90%EB%9F%AC%EB%85%B8%ED%8A%B8-Cant-perform-a-React-state-update-on-an-unmounted-component
    https://coffeeandcakeandnewjeong.tistory.com/89

--- frontend/src/components/pages/BrowseChannel/index.tsx
+++ frontend/src/components/pages/BrowseChannel/index.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useResetRecoilState } from 'recoil';
 import BrowseContent from '@organisms/BrowseContent';
 import WorkspaceTemplate from '@templates/Workspace';
+import { browseCursor } from '@state/Channel';
+import checkIsLogin from '@global/util/CheckIsLogin';
+import { Redirect } from 'react-router-dom';
 
 const BrowseChannel = (): JSX.Element => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    return () => setLoading(false);
+  }, []);
+  if (!checkIsLogin()) {
+    return <Redirect to="/notlogin" />;
+  }
+
   const Content: JSX.Element = <BrowseContent />;
+
+  const resetCursor = useResetRecoilState(browseCursor);
+  useEffect(() => {
+    return () => {
+      resetCursor();
+    };
+  }, []);
 
   return <WorkspaceTemplate Content={Content} />;
 };
