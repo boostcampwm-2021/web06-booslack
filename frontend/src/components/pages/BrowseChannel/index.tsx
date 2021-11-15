128378faf25b955dd380ed8fff15197f37e55c61
blogSoul <blo9040@gmail.com>
2021-11-15 09:38:29 +0900

    feat: 로그인 여부 확인 함수 구현 및 모든 페이지 로그인별 페이지 경고 및 라우터 구현
    1. 로그인 여부 확인 함수 구현함.
    2. 로그인이 필요한 페이지는 로그인한 유저만 들어올 수 있도록 하고 로그인이 필요하지 않은 페이지는 로그인 안한 유저만 들어올 수 있도록 라우터 구현.
    
    에러에 대한 참고 링크 :
    https://velog.io/@ohgoodkim/-%EC%97%90%EB%9F%AC%EB%85%B8%ED%8A%B8-Cant-perform-a-React-state-update-on-an-unmounted-component
    https://coffeeandcakeandnewjeong.tistory.com/89

--- frontend/src/components/pages/BrowseChannel/index.tsx
+++ frontend/src/components/pages/BrowseChannel/index.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useResetRecoilState } from 'recoil';
 import BrowseContent from '@organisms/BrowseContent';
 import WorkspaceTemplate from '@templates/Workspace';
 import { browseCursor } from '@state/Channel';
+import checkIsLogin from '@global/util/CheckIsLogin';
+import { Redirect } from 'react-router-dom';
 
 const BrowseChannel = (): JSX.Element => {
+<<<<<<< HEAD
 <<<<<<< HEAD
   return (
     <WorkspaceTemplate>
@@ -12,6 +15,16 @@ const BrowseChannel = (): JSX.Element => {
     </WorkspaceTemplate>
   );
 =======
+=======
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    return () => setLoading(false);
+  }, []);
+  if (!checkIsLogin()) {
+    return <Redirect to="/notlogin" />;
+  }
+
+>>>>>>> 80e0f89 (feat: 로그인 여부 확인 함수 구현 및 모든 페이지 로그인별 페이지 경고 및 라우터 구현)
   const Content: JSX.Element = <BrowseContent />;
 
   const resetCursor = useResetRecoilState(browseCursor);
--- frontend/src/components/pages/WorkspaceList/index.tsx
+++ frontend/src/components/pages/WorkspaceList/index.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import WorkspaceListTemplate from '@templates/WorkspaceList';
 import WorkSpaceListContent from '@organisms/WorkspaceListContent';
+import checkIsLogin from '@global/util/CheckIsLogin';
+import { Redirect } from 'react-router-dom';
 import { Container } from './styles';
 
 const WorkspaceList = (): JSX.Element => {
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    return () => setLoading(false);
+  }, []);
+  if (!checkIsLogin()) {
+    return <Redirect to="/notlogin" />;
+  }
+
   return (
     <Container>
       <WorkspaceListTemplate>
