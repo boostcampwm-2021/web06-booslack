bc157384d4c87b8a229c7214a14e84072af4f39c
Lee <ycp998@naver.com>
2021-11-17 23:24:07 +0900

    feat : pagenation 에 type 명세화 및 option 코드 추가

--- frontend/src/hooks/usePagenation.ts
+++ frontend/src/hooks/usePagenation.ts
@@ -1,18 +1,29 @@
-import { useEffect, useState } from 'react';
+import { SetStateAction, Dispatch, useEffect, useState } from 'react';
 import { useQuery, useQueryClient } from 'react-query';
+
+interface IusePagenation {
+  page: number;
+  setPage: Dispatch<SetStateAction<number>>;
+  isLoading: boolean;
+  data: { hasMore: boolean };
+  error: unknown;
+  isFetching: boolean;
+  isPreviousData: boolean;
+}
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const usePagenation = (
   key: string,
-  axiosFunction: (page: number) => Promise<any>,
-) => {
+  axiosFunction: (page: number) => Promise<unknown & { hasMore: boolean }>,
+  option?: typeof Object,
+): IusePagenation => {
   const queryClient = useQueryClient();
   const [page, setPage] = useState<number>(0);
 
   const { isLoading, data, error, isFetching, isPreviousData } = useQuery(
     [key, page],
     () => axiosFunction(page),
-    { keepPreviousData: true, staleTime: 5000 },
+    { keepPreviousData: true, staleTime: 5000, ...option },
   );
 
   useEffect(() => {
