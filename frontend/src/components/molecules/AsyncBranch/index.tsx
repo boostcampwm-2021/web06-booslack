c461d53743fb576821c031dc111c48d1f51e7c3f
Lee <ycp998@naver.com>
2021-11-19 04:11:43 +0900

    feat: 오류 메세지 생성

--- frontend/src/components/molecules/AsyncBranch/index.tsx
+++ frontend/src/components/molecules/AsyncBranch/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { ThemeContext } from 'styled-components';
 import { InfiniteData } from 'react-query';
 import { Data } from '@hook/useAsync';
+import Container, { LoadingSpinner, MarginDiv } from './styles';
 
 interface Props {
   data: Data | unknown[] | InfiniteData<unknown>;
@@ -15,8 +17,19 @@ const AsyncBranch = ({
   data,
   children,
 }: Props): JSX.Element => {
+  const themeContext = useContext(ThemeContext);
+  const color = themeContext.backgroundColor;
+
   if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
+  if (error) {
+    return (
+      <Container>
+        에러가 발생했습니다.
+        <MarginDiv />
+        <LoadingSpinner color={color} />
+      </Container>
+    );
+  }
   if (!data) return <></>;
   return <>{children}</>;
 };
