7ce902d208c0a94367f429eb74a3766d3adf8464
laz <danier123@gmail.com>
2021-11-23 16:00:06 +0900

    feat: userState의props 정의

--- frontend/src/state/user/index.ts
+++ frontend/src/state/user/index.ts
@@ -1,6 +1,24 @@
 import { atom } from 'recoil';
+import { Socket } from 'socket.io-client';
+import { Itheme } from '@global/theme';
 
-const userState = atom({
+type ZeroOrOne = 1 | 2;
+
+interface Props {
+  account: string;
+  createdAt: Date;
+  description: string;
+  email: string;
+  id: number;
+  local: ZeroOrOne;
+  nickname: string;
+  password: string;
+  socket: Socket;
+  theme: Itheme;
+  userHasWorkspaceId: number;
+}
+
+const userState = atom<Props | undefined>({
   key: 'userState',
   default: undefined,
   dangerouslyAllowMutability: true,
