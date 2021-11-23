d0cbe0bffd0ec6c14bfb8224e56dc8863d203c22
laz <danier123@gmail.com>
2021-11-23 11:05:34 +0900

    feat: 채널 브라우저에서 채널 생성 버튼

--- frontend/src/components/organisms/BrowseContent/index.tsx
+++ frontend/src/components/organisms/BrowseContent/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useSetRecoilState } from 'recoil';
 import Label from '@atoms/Label';
 import LabeledDefaultButton from '@atoms/LabeledDefaultButton';
 import BrowseChannelList from '@organisms/BrowseChannelList';
+import { channelCreateModalState } from '@state/modal';
 import { Container, StyledBrowseChannelHeader } from './styles';
 
 const BrowseContent = (): JSX.Element => {
+  const setIsOpen = useSetRecoilState(channelCreateModalState);
+
   const Title: JSX.Element = <Label text="채널 브라우저" />; // to-do React.memo
-  const RightButton = <LabeledDefaultButton onClick={() => {}} text="btn" />;
+  const RightButton = (
+    <LabeledDefaultButton
+      onClick={() => {
+        setIsOpen(true);
+      }}
+      text="Create Channel"
+    />
+  );
 
   return (
     <Container>
