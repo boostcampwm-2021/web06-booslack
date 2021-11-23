d58c934ee11168d7e3cf16a87fadcd6f62a4d2bf
laz <danier123@gmail.com>
2021-11-23 17:55:19 +0900

    feat: 처음 채널 로딩시 제일 하단으로 스크롤 - 제일 상단으로 올라갔을 때 감지

--- frontend/src/components/organisms/ChatContent/index.tsx
+++ frontend/src/components/organisms/ChatContent/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import ThreadContent from '@molecules/ThreadContent';
 import { useThreadListQuery } from '@hook/useThreads';
@@ -12,8 +12,29 @@ interface Props {
 
 const ChatContent = ({ inputBar, channelName }: Props): JSX.Element => {
   const { channelId }: { channelId: string } = useParams();
+  const topRef = useRef(null);
+  const bottomRef = useRef(null);
 
   const { isLoading, isError, data: threads } = useThreadListQuery(channelId);
+
+  useEffect(() => {
+    if (!threads) return;
+
+    const observer = new IntersectionObserver((entries) =>
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          // fetch
+        }
+      }),
+    );
+    observer.observe(topRef.current);
+  }, [threads]);
+
+  // on every new message
+  useEffect(() => {
+    if (!threads) return;
+    bottomRef.current?.scrollIntoView({ block: 'end' });
+  }, [threads]);
 
   if (isLoading) return <div>Loading</div>;
   if (isError) return <div>Error</div>;
@@ -28,6 +49,7 @@ const ChatContent = ({ inputBar, channelName }: Props): JSX.Element => {
             channelName={channelName}
           />
         ))}
+        <div ref={bottomRef} />
       </Container>
       {inputBar}
     </>
