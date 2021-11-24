079244db09f4eb2afeda2d49b770be643517e498
blogSoul <blo9040@gmail.com>
2021-11-25 00:51:10 +0900

    fix: rebase하면서 잘못된 부분 수정
    1. rebase하면서 잘못된 부분 수정함.

--- frontend/src/components/pages/BrowseChannel/index.tsx
+++ frontend/src/components/pages/BrowseChannel/index.tsx
@@ -3,24 +3,11 @@ import BrowseContent from '@organisms/BrowseContent';
 import WorkspaceTemplate from '@templates/Workspace';
 
 const BrowseChannel = (): JSX.Element => {
-<<<<<<< HEAD
   return (
     <WorkspaceTemplate>
       <BrowseContent />
     </WorkspaceTemplate>
   );
-=======
-  const Content: JSX.Element = <BrowseContent />;
-
-  const resetCursor = useResetRecoilState(browseCursor);
-  useEffect(() => {
-    return () => {
-      resetCursor();
-    };
-  }, []);
-
-  return <WorkspaceTemplate Content={Content} />;
->>>>>>> 0b1db23 (fix: browserchannel과 client에 대한 오류 수정)
 };
 
 export default BrowseChannel;
