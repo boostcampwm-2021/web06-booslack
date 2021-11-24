468d63e218fef49ff05ee3085718ab47092a7241
j166_이충헌 <ycp998@naver.com>
2021-11-25 00:21:49 +0900

    feat : thread 헤더 구현

--- frontend/src/components/organisms/ReplyBar/index.tsx
+++ frontend/src/components/organisms/ReplyBar/index.tsx
@@ -1,7 +1,47 @@
-import React, { useContext } from 'react';
+/* eslint-disable react/jsx-wrap-multilines */
+import React, { useMemo } from 'react';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { useParams } from 'react-router-dom';
+
+import Label from '@atoms/Label';
 import { replyToggleState, replyWorkspaceState } from '@state/workspace';
-import Container from './styles';
+import { useChannelQuery } from '@hook/useChannels';
+import { CHANNELTYPE } from '@enum/index';
+import xMarkImage from '@global/image/xMark.png';
+import Container, {
+  StyledBrowseChannelHeader,
+  StyledLabel,
+  Xdiv,
+} from './styles';
+import ImageButton from '@atoms/ImageButton';
+
+const ReplyHeader = (): JSX.Element => {
+  const LeftSizeLabel = () => useMemo(() => <StyledLabel text="쓰레드" />, []);
+
+  const { channelId }: { channelId: string } = useParams();
+  const { isError, data } = useChannelQuery(channelId);
+
+  if (isError) return <div>Error</div>;
+
+  const channelName = data?.name?.substr(0, 10);
+  const channelType = CHANNELTYPE[data?.private];
+
+  return (
+    <StyledBrowseChannelHeader
+      title={
+        <div>
+          <LeftSizeLabel />
+          <span> </span>
+          <Label color="grey" text={channelType} />
+          <span> </span>
+          <Label color="grey" text={channelName} />
+        </div>
+      }
+      content={<></>}
+      rightButton={<Xdiv />}
+    />
+  );
+};
 
 const ReplyBar = (): JSX.Element => {
   const SIZEVW = useRecoilValue(replyWorkspaceState);
@@ -9,7 +49,7 @@ const ReplyBar = (): JSX.Element => {
 
   return (
     <Container widthVW={SIZEVW} isOpened={isOpened}>
-      <></>
+      <ReplyHeader />
     </Container>
   );
 };
--- frontend/src/components/organisms/ReplyBar/styles.ts
+++ frontend/src/components/organisms/ReplyBar/styles.ts
@@ -1,17 +1,38 @@
 import styled from 'styled-components';
 import { scrollIfHover } from '@global/mixin';
+import BrowseChannelHeader from '@molecules/BrowseChannelHeader';
+import Label from '@atoms/Label';
 
 const Container = styled.div<{ widthVW: number; isOpened: boolean }>`
   display: ${({ isOpened }) => (isOpened ? 'flex' : 'none')};
+  flex-direction: column;
   width: ${({ widthVW }) => widthVW}vw;
   height: 100vh;
   background-color: red;
 
+  border-left: 1px solid #f8f8f8;
   ${scrollIfHover}
+`;
+
+export const StyledBrowseChannelHeader = styled(BrowseChannelHeader)`
+  --saf-0: rgba(var(--sk_foreground_low, 29, 28, 29), 0.13);
+  box-shadow: 0 0 0 1px var(--saf-0), 0 4px 12px 0 rgba(0, 0, 0, 0.12);
+`;
+
+export const StyledLabel = styled(Label)`
+  font-weight: bold;
 `;
 
 export const MarginDiv = styled.div`
   margin-top: 20vh;
+`;
+
+export const Xdiv = styled.div`
+  width: 30px;
+  height: 30px;
+  &:before {
+    content: '\E278';
+  }
 `;
 
 export default Container;
