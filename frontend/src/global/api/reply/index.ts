cc870478a32d9fc7e11ac6021dbf262473184ca7
blogSoul <blo9040@gmail.com>
2021-11-28 14:28:04 +0900

    feat: 프론트 파트에 global/api/reply file에 필요한 api 함수 추가
    1. 프론트 파트에 global/api/reply file에 필요한 api 함수 추가함.
    
    socket 이벤트는 threads로 처리되어 있으니 변경하면 됩니다.

---
+++ frontend/src/global/api/reply/index.ts
@@ -0,0 +1,107 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import axios from 'axios';
+
+export const updateReply = async (
+  replyId: string,
+  channelId: string,
+  message: string,
+  socket,
+  setUpdateState: Dispatch<SetStateAction<boolean>>,
+): Promise<any> => {
+  const res = await axios.put(`/api/replys/${replyId}`, {
+    message,
+  });
+  if (res.status === 200) {
+    setUpdateState(false);
+    socket.emit('threads', channelId, replyId);
+  }
+};
+
+export const deleteReply = async (
+  replyId: string,
+  channelId: string,
+  socket,
+): Promise<any> => {
+  const res = await axios.delete(`/api/replys/${replyId}`);
+  if (res.status === 200) {
+    socket.emit('threads', channelId, replyId);
+  }
+};
+
+export const postReply = async (
+  userHasWorkspaceId: string,
+  threadId: string,
+  channelId: string,
+  message: string,
+  socket,
+  setMessageClear: Dispatch<SetStateAction<boolean>>,
+): Promise<any> => {
+  const res = await axios.post('/api/replys', {
+    userHasWorkspaceId,
+    message,
+    threadId,
+  });
+  if (res.status === 200) {
+    setMessageClear(true);
+    socket.emit('threads', channelId);
+  }
+};
+
+export const postReplyAndFiles = async (
+  userHasWorkspaceId: string,
+  threadId: string,
+  channelId: string,
+  message: string,
+  socket,
+  setMessageClear: Dispatch<SetStateAction<boolean>>,
+  setMessage,
+  selectedFile: any,
+  setSelectedFile,
+  setSelectedFileUrl,
+  setShouldScrollDown,
+): Promise<any> => {
+  if (message === '<p><br/></p>' && selectedFile.length === 0) return;
+  const res = await axios.post('/api/replys', {
+    userHasWorkspaceId,
+    message,
+    threadId,
+  });
+  if (res.status === 200) {
+    setMessageClear(true);
+    socket.emit('threads', channelId);
+    if (res.data.reply.userHasWorkspaceId === userHasWorkspaceId) {
+      setShouldScrollDown(true);
+    }
+  }
+
+  const replyId: number = res.data.reply.id || null;
+  let fileUrl = '/api/files/upload';
+  const formDatas = new FormData();
+  const selectedFileLength = selectedFile.length;
+
+  if (selectedFileLength > 1) fileUrl = '/api/files/uploads';
+  if (selectedFileLength === 0) return;
+  // eslint-disable-next-line array-callback-return
+  selectedFile.map((fileElement) => {
+    formDatas.append('file', fileElement);
+  });
+  setSelectedFile([]);
+  setSelectedFileUrl([]);
+
+  const config = {
+    headers: { 'content-type': 'multipart/form-data' },
+  };
+  axios
+    .post(fileUrl, formDatas, config)
+    // eslint-disable-next-line @typescript-eslint/no-shadow
+    .then((response) => {
+      const fileList: Array<number> = response.data.files;
+      // eslint-disable-next-line array-callback-return
+      fileList.map(async (fileId) => {
+        await axios.put(`/api/files/${fileId}`, { replyId });
+      });
+    })
+    .catch((err) => {
+      throw new Error('No files Error');
+    });
+};
