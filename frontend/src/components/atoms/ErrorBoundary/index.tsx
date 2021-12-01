8e198aa1b811a62dc299860a743d0391a1e4c91c
laz <danier123@gmail.com>
2021-12-02 00:32:41 +0900

    feat: spinner와 error spinner 아톰 구현

---
+++ frontend/src/components/atoms/ErrorBoundary/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // console.log(error, errorInfo);
+  }
+
+  render() {
+    const { hasError } = this.state;
+    const { children } = this.props;
+
+    if (hasError) {
+      return this.props.fallback;
+    }
+    return children;
+  }
+}
+
+export default ErrorBoundary;
--- frontend/src/components/atoms/Spinner/index.tsx
+++ frontend/src/components/atoms/Spinner/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
-import { DotLoader } from 'react-spinners';
-import styled from 'styled-components';
-
-const Center = styled.div`
-  display: flex;
-  justify-content: center;
-`;
+import { DotLoader, PacmanLoader } from 'react-spinners';
+import { Center, ErrorContainer, PacmanContainer } from './style';
 
 export const SmallSpinner = (): JSX.Element => {
   return (
@@ -16,9 +11,56 @@ export const SmallSpinner = (): JSX.Element => {
 };
 
 export const MediumSpinner = (): JSX.Element => {
-  return <DotLoader size={150} />;
+  return (
+    <Center>
+      <DotLoader size={150} />
+    </Center>
+  );
 };
 
 export const LargeSpinner = (): JSX.Element => {
-  return <DotLoader size={300} />;
+  return (
+    <Center>
+      <DotLoader size={300} />
+    </Center>
+  );
+};
+
+export const SmallErrorSpinner = (): JSX.Element => {
+  return (
+    <div>
+      <PacmanContainer>
+        <PacmanLoader />
+      </PacmanContainer>
+      <ErrorContainer>
+        <h1>오류가 발생했습니다. 다시 시도해주세요.</h1>
+      </ErrorContainer>
+    </div>
+  );
+};
+
+export const MediumErrorSpinner = (): JSX.Element => {
+  return (
+    <div>
+      <PacmanContainer size={50}>
+        <PacmanLoader size={50} />
+      </PacmanContainer>
+      <ErrorContainer>
+        <h1>오류가 발생했습니다. 다시 시도해주세요.</h1>
+      </ErrorContainer>
+    </div>
+  );
+};
+
+export const LargeErrorSpinner = (): JSX.Element => {
+  return (
+    <div>
+      <PacmanContainer size={100}>
+        <PacmanLoader size={100} />
+      </PacmanContainer>
+      <ErrorContainer>
+        <h1>오류가 발생했습니다. 다시 시도해주세요.</h1>
+      </ErrorContainer>
+    </div>
+  );
 };
