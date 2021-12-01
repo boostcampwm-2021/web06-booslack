a7cbebb2eb6459b840c8dcdc875af283b4f0c533
blogSoul <blo9040@gmail.com>
2021-12-01 09:27:23 +0900

    feat: UserProfileModal 이미지 연동
    1. UserProfileModal 이미지 연동함.

--- frontend/src/components/organisms/UserProfileModal/index.tsx
+++ frontend/src/components/organisms/UserProfileModal/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRecoilState } from 'recoil';
 import { userProfileModalState } from '@state/modal';
 import Label from '@atoms/Label';
-import mintImage from '@global/image/mintChoco.png';
+import defaultAccount from '@global/image/default_account.png';
+import axios from 'axios';
 import {
   StyledPopup,
   Container,
@@ -18,6 +19,17 @@ const UserProfileModal = (): JSX.Element => {
   const [{ isOpen, userHasWorkspace, x, y }, setIsOpen] = useRecoilState(
     userProfileModalState,
   );
+  const [fileUrl, setFileUrl] = useState(defaultAccount);
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  useEffect(async () => {
+    if (userHasWorkspace.fileId && userHasWorkspace.fileId !== 1) {
+      const response = await axios.get(`/api/files/${userHasWorkspace.fileId}`);
+      if (response?.data && response?.data.files && response?.data.files.url) {
+        setFileUrl(response?.data.files.url);
+      }
+    }
+  }, []);
 
   return (
     <StyledPopup
@@ -28,8 +40,7 @@ const UserProfileModal = (): JSX.Element => {
         setIsOpen({ isOpen: false, userHasWorkspace: null, x: null, y: null })
       }
     >
-      {/* TODO: user profile picture */}
-      <StyledImageBox image={mintImage} />
+      <StyledImageBox image={fileUrl} />
       <Container>
         <HeaderContainer>
           <StyledBoldLabel text={userHasWorkspace.nickname} />
